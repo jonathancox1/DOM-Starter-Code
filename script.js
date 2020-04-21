c558f5f7f2749674b2c3c0fd46cb3fce582d080c
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-21 19:30:07 -0400

    updated error handling

--- script.js
+++ script.js
@@ -13,17 +13,18 @@ const heading = document.getElementsByTagName("span");
 
 
 myButton.addEventListener('click', function (e) {
-    // prevents the default response to the 'click' event
-    // which would submit and refresh the page
-    e.preventDefault();
-    // reassigning noun1.innerHTML
-    name.innerHTML = nameInput.value;
-    defMsg.style.display = 'none';
-    email.textContent = 'Your email: ' + (emailInput.value);
-    age.innerHTML = 'Your age: ' + (ageInput.value);
-    document.getElementById('myForm').style.display = 'none';
-    document.getElementById('invisible').style.display = 'block';
+    if (nameInput.value == '' || emailInput.value == '' || ageInput.value == '') {
+        alert('You must enter all fields');
+    } else {
+        // prevents the default response to the 'click' event
+        // which would submit and refresh the page
+        e.preventDefault();
+        // reassigning noun1.innerHTML
+        name.innerHTML = nameInput.value;
+        defMsg.style.display = 'none';
+        email.textContent = 'Your email: ' + (emailInput.value);
+        age.innerHTML = 'Your age: ' + (ageInput.value);
+        document.getElementById('myForm').style.display = 'none';
+        document.getElementById('invisible').style.display = 'block';
+    }
 });
-
-
-//style.display = 'none';
